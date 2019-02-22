4b5a2be5db5f08c4d8f6e0cfadc6c7857f121af4
Idan Madar <t882200@gmail.com>
2019-02-22 08:17:55 +0000

    add docs for gh-pages

---
+++ docs/js/main.js
@@ -0,0 +1,74 @@
+const url = '../docs/pdf.pdf';
+
+let pdfDoc = null, pageNum = 1, pageIsRendering = false, pageNumIsPending = null;
+
+const scale = 1.5, canvas = document.querySelector('#pdf-render'), ctx = canvas.getContext('2d');
+
+// render the page
+const renderPage = num => {
+    pageIsRendering = true;
+    
+    // get page
+    pdfDoc.getPage(num).then(page => {
+        // set scale
+        const viewport = page.getViewport({ scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        
+        const renderCtx = {
+            canvasContext:  ctx,
+            viewport
+        }
+        page.render(renderCtx).promise.then(() => {
+           pageIsRendering = false;
+           
+           if(pageNumIsPending !== null){
+               renderPage(pageNumIsPending);
+               pageNumIsPending = null;
+           }
+        });
+        
+        // output current page
+        document.querySelector('#page-num').textContent = num;
+    });
+};
+
+// check for page rendering
+const queueRenderPage = num => {
+    if(pageIsRendering) {
+        pageNumIsPending = num;
+    } else {
+        renderPage(num);
+    }
+}
+
+//  show prev page
+const showPrevPage = () => {
+    if(pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
+//  show next page
+const showNextPage = () => {
+    if(pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+}
+
+// get the doc
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+    
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+    
+    renderPage(pageNum)
+});
+
+// btn events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
